ddc79cc70068f2b27e3d4d888b2d39083c9389a7
pilade <pilade.franceschi@mapsgroup.it>
2022-01-07 17:10:09 +0100

    format classes

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,26 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
-     * 
+     *
      * @return Response
      */
     public function indexAction(Request $request)
     {
         $response = new Response();
         $yaml = file_get_contents('../musement.weather.api.yml');
-        $response->setContent(($yaml !== false) ? $yaml : null);
+        $response->setContent((false !== $yaml) ? $yaml : null);
         $response->headers->set('Content-Type', 'text/plain');
-        
+
         return $response;
     }
-        
 }
--- tests/App/Service/WeatherServiceTest.php
+++ tests/App/Service/WeatherServiceTest.php
@@ -2,40 +2,40 @@
 
 namespace Tests\App\Service;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use App\Dto\City;
-use App\Service\WeatherService;
 use App\Dto\Weather;
+use App\Service\WeatherService;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class WeatherServiceTest extends WebTestCase
 {
-    
-    public function testGetCities() {
-        
+    public function testGetCities()
+    {
         $weatherService = new WeatherService();
         $cities = $weatherService->getCities();
-        
+
         $this->assertIsArray($cities);
-        
+
         foreach ($cities as $city) {
             $this->assertInstanceOf(City::class, $city);
             // test only one item
             break;
         }
     }
-    
-    public function testGetWeather() {
+
+    public function testGetWeather()
+    {
         $weatherService = new WeatherService();
-        
+
         // Amsterdam
         $latitude = 52.37;
         $longitude = 4.9;
         $days = 2;
-        
+
         $weathers = $weatherService->getWeather($latitude, $longitude, $days);
-        
+
         $this->assertIsArray($weathers);
-        
+
         foreach ($weathers as $weather) {
             $this->assertInstanceOf(Weather::class, $weather);
         }
