6f2b7f695c2259d480a77311ee6b1c6e5cf52fd4
pilade <pilade.franceschi@mapsgroup.it>
2022-01-07 14:32:42 +0100

    added WeatherService Tests

---
+++ tests/App/Service/WeatherServiceTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\App\Service;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Dto\City;
+use App\Service\WeatherService;
+use App\Dto\Weather;
+
+class WeatherServiceTest extends WebTestCase
+{
+    
+    public function testGetCities() {
+        
+        $weatherService = new WeatherService();
+        $cities = $weatherService->getCities();
+        
+        $this->assertIsArray($cities);
+        
+        foreach ($cities as $city) {
+            $this->assertInstanceOf(City::class, $city);
+            // test only one item
+            break;
+        }
+    }
+    
+    public function testGetWeather() {
+        $weatherService = new WeatherService();
+        
+        // Amsterdam
+        $latitude = 52.37;
+        $longitude = 4.9;
+        $days = 2;
+        
+        $weathers = $weatherService->getWeather($latitude, $longitude, $days);
+        
+        $this->assertIsArray($weathers);
+        
+        foreach ($weathers as $weather) {
+            $this->assertInstanceOf(Weather::class, $weather);
+        }
+    }
+}
